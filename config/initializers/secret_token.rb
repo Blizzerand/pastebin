317387acfaaa2e64e8c305b495b0a7c1439a8b72
SanTrans <anon.wiki04@gmail.com>
2013-06-05 00:08:09 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pastebin::Application.config.secret_token = 'c7585347391de06425c6e0b399cf6eab282f1f0a1436a7d7c926484d4f7243dd1b98abf38f7d55747e59568bc08e4d4c29a5f460a846c901c97d4bbe6b9242aa'
