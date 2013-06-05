64b5e316536b10d2b4f4517d359232284f1e351b
SanTrans <anon.wiki04@gmail.com>
2013-06-05 18:06:08 +0530

    Tiny edits

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+		def full_title(page_title)
+		base_title = "Sample RoR Pastebin"
+		if page_title.blank?
+			base_title
+		else 
+		 	"#{base_title} | #{page_title}"
+		end
+	end
 end
