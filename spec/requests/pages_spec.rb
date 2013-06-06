5f4bff114535115753795921ae0e4691229d8488
SanTrans <anon.wiki04@gmail.com>
2013-06-06 11:46:19 +0530

    Finish layout

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -7,8 +7,8 @@ describe "Pages" do
   describe "Pasteit" do
   	before { visit pasteit_path }
 
-  	it { should have_selector('title', text: full_title("Pasteit!")) }
-  	it { should have_selector('h1', text: "Pasteit!") }
+  	it { should have_selector('title', text: full_title("Paste it!")) }
+  	it { should have_selector('h1', text: "Paste it!") }
   end 
 end
  
